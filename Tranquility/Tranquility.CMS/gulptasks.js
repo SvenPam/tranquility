7affe8f968542ad3a5c31951182555977da89448
Stephen Pammenter <me@ste-pam.com>
2016-11-20 16:17:25 +0000

    Renamed directory to match project name...

---
+++ Tranquility/Tranquility.CMS/gulptasks.js
@@ -0,0 +1,104 @@
+﻿/// <binding ProjectOpened='watch' />
+const gulp = require('gulp');
+const changed = require('gulp-changed');
+const clean = require('gulp-clean');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const cleanCSS = require('gulp-clean-css');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const runSequence = require('run-sequence');
+const rename = require("gulp-rename");
+const imagemin = require('gulp-imagemin');
+const imageminJpegRecompress = require('imagemin-jpeg-recompress');
+
+const projectProperties =
+{
+	assetDirectory: 'a',
+	sourceName: 'Src',
+	distName: ''
+}
+const path = {
+	build: {
+		style: `${projectProperties.assetDirectory + projectProperties.distName}/Styles`,
+		js: `${projectProperties.assetDirectory + projectProperties.distName}/Scripts`,
+		img: `${projectProperties.assetDirectory + projectProperties.distName}/Images`,
+		video: `${projectProperties.assetDirectory + projectProperties.distName}/Videos`,
+		config: `${projectProperties.assetDirectory + projectProperties.distName}`
+	},
+	src: {
+		style: `${projectProperties.assetDirectory + projectProperties.sourceName}/css`,
+		js: `${projectProperties.assetDirectory + projectProperties.sourceName}/js`,
+		img: `${projectProperties.assetDirectory + projectProperties.sourceName}/img`,
+		video: `${projectProperties.assetDirectory + projectProperties.sourceName}/vid`,
+		config: `${projectProperties.assetDirectory + projectProperties.sourceName}`
+	},
+	clean: `${projectProperties.assetDirectory + projectProperties.distName}`
+};
+
+gulp.task('default', () => {
+	runSequence('clean', ['styles', 'images', 'scripts', 'externalScripts', 'videos', 'configs']);
+});
+gulp.task('clean', () =>
+	gulp.src([`${path.clean}/*`, `!${path.build.img}`], { read: false })
+        .pipe(clean({ force: true }))
+);
+gulp.task('styles', () =>
+	gulp.src([`${path.src.style}/main.scss`], [`${path.src.style}/main.scss`])
+        .pipe(sourcemaps.init())
+        .pipe(changed(path.build.style))
+        .pipe(sass().on('error', sass.logError))
+        .pipe(cleanCSS())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest(path.build.style))
+);
+gulp.task('images', () =>
+	gulp.src(`${path.src.img}/**/**.**`)
+        .pipe(changed(path.build.img))
+        .pipe(imagemin([
+            imageminJpegRecompress({
+            	quality: 'low',
+            	progressive: true
+            }),
+            imagemin.gifsicle(),
+            imagemin.optipng(),
+            imagemin.svgo()
+        ]))
+        .pipe(gulp.dest(path.build.img))
+);
+gulp.task('scripts', () => {
+	return gulp.src([`${path.src.js}/**.js`])
+        .pipe(sourcemaps.init())
+        .pipe(concat('site.js'))
+        .pipe(uglify())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest(path.build.js))
+});
+gulp.task('externalScripts', () => {
+	return gulp.src([`${path.src.js}/external/**.js`])
+        .pipe(sourcemaps.init())
+        .pipe(concat('external.js'))
+        .pipe(uglify())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest(path.build.js))
+});
+
+gulp.task('configs', () =>
+	gulp.src(`${path.src.config}/Web.config`)
+        .pipe(gulp.dest(path.build.config))
+);
+
+gulp.task('videos', () =>
+	gulp.src(`${path.src.video}/**/**.**`)
+        .pipe(changed(path.build.video))
+        .pipe(gulp.dest(path.build.video))
+);
+gulp.task('watch', () => {
+	gulp.watch(`${path.src.style}/**/*.scss`, ['styles']);
+	gulp.watch(`${path.src.img}/**/**.**`, ['images']);
+	gulp.watch(`${path.src.js}/**.js`, ['scripts']);
+	gulp.watch(`${path.src.js}/external/**.js`, ['externalScripts']);
+});
