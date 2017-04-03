9cb95ec43efbdfeb7abdd0f0e3c5343503231c9f
Stephen Pammenter <spammenter@live.com>
2017-04-03 21:44:54 +0100

    Cleaned up gulp tasks some more.

--- Tranquility.CMS/gulpfile.js
+++ Tranquility.CMS/gulpfile.js
@@ -12,14 +12,14 @@ const gulp = require('gulp'),
 // Custom project tasks.
 
 gulp.task('lint-sass', function () {
-	return gulp.src(`${pckg.paths.src.styles}/**/*.s+(a|c)ss`)
+    return gulp.src(`${pckg.paths.src.styles}/**/*.s+(a|c)ss`)
         .pipe($.sassLint(pckg.sassLintConfig))
         .pipe($.sassLint.format())
         .pipe($.sassLint.failOnError());
 });
 
 gulp.task('lint-js', function () {
-	return gulp.src(`${pckg.paths.src.styles}/**/*.js`)
+    return gulp.src(`${pckg.paths.src.styles}/**/*.js`)
         .pipe($.jshint(pckg.jshintConfig))
         .pipe($.jshint.reporter('default'))
         .pipe($.jshint.reporter('fail'))
@@ -27,43 +27,25 @@ gulp.task('lint-js', function () {
 
 
 gulp.task('styles', function () {
-	return gulp.src(`${pckg.paths.src.styles}/*.s+(a|c)ss`)
+    return gulp.src(`${pckg.paths.src.styles}/*.s+(a|c)ss`)
         .pipe($.changed('./'))
         .pipe($.sass().on('error', $.sass.logError))
         .pipe($.autoprefixer({
-        	browsers: [
-          //
-          // Official browser support policy:
-          // https://v4-alpha.getbootstrap.com/getting-started/browsers-devices/#supported-browsers
-          //
-          'Chrome >= 35', // Exact version number here is kinda arbitrary
-          // Rather than using Autoprefixer's native "Firefox ESR" version specifier string,
-          // we deliberately hardcode the number. This is to avoid unwittingly severely breaking the previous ESR in the event that:
-          // (a) we happen to ship a new Bootstrap release soon after the release of a new ESR,
-          //     such that folks haven't yet had a reasonable amount of time to upgrade; and
-          // (b) the new ESR has unprefixed CSS properties/values whose absence would severely break webpages
-          //     (e.g. `box-sizing`, as opposed to `background: linear-gradient(...)`).
-          //     Since they've been unprefixed, Autoprefixer will stop prefixing them,
-          //     thus causing them to not work in the previous ESR (where the prefixes were required).
-          'Firefox >= 38', // Current Firefox Extended Support Release (ESR); https://www.mozilla.org/en-US/firefox/organizations/faq/
-          // Note: Edge versions in Autoprefixer & Can I Use refer to the EdgeHTML rendering engine version,
-          // NOT the Edge app version shown in Edge's "About" screen.
-          // For example, at the time of writing, Edge 20 on an up-to-date system uses EdgeHTML 12.
-          // See also https://github.com/Fyrd/caniuse/issues/1928
-          'Edge >= 12',
-          'Explorer >= 10',
-          // Out of leniency, we prefix these 1 version further back than the official policy.
-          'iOS >= 8',
-          'Safari >= 8',
-          // The following remain NOT officially supported, but we're lenient and include their prefixes to avoid severely breaking in them.
-          'Android 2.3',
-          'Android >= 4',
-          'Opera >= 12'
-        	]
+            browsers: [
+                'Chrome >= 35',
+                'Firefox >= 38',
+                'Edge >= 12',
+                'Explorer >= 10',
+                'iOS >= 8',
+                'Safari >= 8',
+                'Android 2.3',
+                'Android >= 4',
+                'Opera >= 12'
+            ]
         }))
         .pipe($.cleanCss({ debug: true, semanticMerging: true }, function (details) {
-        	console.log(`Original ${details.name}: ${details.stats.originalSize}`);
-        	console.log(`Cleaned ${details.name}: ${details.stats.minifiedSize} (${Math.round((details.stats.minifiedSize / details.stats.originalSize) * 100)}%)`);
+            console.log(`Original ${details.name}: ${details.stats.originalSize}`);
+            console.log(`Cleaned ${details.name}: ${details.stats.minifiedSize} (${Math.round((details.stats.minifiedSize / details.stats.originalSize) * 100)}%)`);
         }))
         .pipe($.rename({ suffix: '.min' }))
         .pipe($.sourcemaps.write('./'))
@@ -71,17 +53,14 @@ gulp.task('styles', function () {
 })
 
 gulp.task('scripts', function () {
-	return gulp.src(`${pckg.paths.src.styles}/**/*.js`)
+    return gulp.src(`${pckg.paths.src.styles}/**/*.js`)
         .pipe($.babel({
-        	presets: ['es2015']
+            presets: ['es2015']
         }))
         .pipe($.sourcemaps.write('./'))
         .pipe(gulp.dest(pckg.paths.dist.scripts));
 });
 
-/******************************************************
- * COPY TASKS - stream assets from source to destination
-******************************************************/
 // Copy external frameworks over.
 gulp.task('external-js', function () {
     return gulp.src(externalJs.references)
@@ -90,7 +69,7 @@ gulp.task('external-js', function () {
 
 // Images copy
 gulp.task('img', function () {
-	return gulp.src(`${pckg.paths.src.images}/**/*.*`)
+    return gulp.src(`${pckg.paths.src.images}/**/*.*`)
         .pipe($.changed(pckg.paths.dist.images))
         .pipe($.imagemin())
         .pipe(gulp.dest(pckg.paths.dist.images));
@@ -102,21 +81,21 @@ gulp.task('css', gulp.series('lint-sass', 'styles'));
 gulp.task('js', gulp.series('lint-js', 'scripts'));
 
 gulp.task('build', gulp.series(
-  gulp.parallel(
-    'css',
-    'js',
-    'external-js',
-    'img'
-  ),
-  function (done) {
-  	done();
-  })
+    gulp.parallel(
+        'css',
+        'js',
+        'external-js',
+        'img'
+    ),
+    function (done) {
+        done();
+    })
 );
 
 function watch() {
-	gulp.watch(`${pckg.paths.src.styles}/**/*.scss`, { awaitWriteFinish: true }).on('change', gulp.series('css'));
-	gulp.watch(`${pckg.paths.src.js}/**/*.js`, { awaitWriteFinish: true }).on('change', gulp.series('js'));
-	gulp.watch(`${pckg.paths.src.images}/**/*.*`, { awaitWriteFinish: true }).on('change', gulp.series('img'));
+    gulp.watch(`${pckg.paths.src.styles}/**/*.scss`, { awaitWriteFinish: true }).on('change', gulp.series('css'));
+    gulp.watch(`${pckg.paths.src.js}/**/*.js`, { awaitWriteFinish: true }).on('change', gulp.series('js'));
+    gulp.watch(`${pckg.paths.src.images}/**/*.*`, { awaitWriteFinish: true }).on('change', gulp.series('img'));
 }
 
 /******************************************************
