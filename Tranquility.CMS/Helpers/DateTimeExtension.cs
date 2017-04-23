0116c67d967510d9f9ae06d7fd47acdfc02aed64
Ste Pammenter <spammenter@live.com>
2017-04-23 15:50:00 +0100

    Added blog.

---
+++ Tranquility.CMS/Helpers/DateTimeExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Tranquility.Helpers
+{
+    public static class DateTimeExtension
+    {
+        public static string Friendly(this DateTime date)
+        {
+            if (date.Date == DateTime.Now.Date)
+            {
+                return "Today at " + date.ToString("HH:MM");
+            }
+            else if (date.Date > DateTime.Now.AddDays(-6))
+            {
+                return date.DayOfWeek.ToString();
+            }
+
+            return date.ToString("dd-MMM-yy");
+        }
+    }
+}
