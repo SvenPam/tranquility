75c9f63a3ab89d8f90504feba07c1ee9aa9a2c33
Ste Pammenter <spammenter@live.com>
2017-04-29 18:04:11 +0100

    Added better email service.

--- Tranquility.CMS/Service/NotificationService.cs
+++ Tranquility.CMS/Service/NotificationService.cs
@@ -1,24 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Net.Mail;
-using System.Web;
 using Tranquility.Service.Interface;
 using Tranquility.ViewModel;
+using Umbraco.Core.Services;
 
 namespace Tranquility.Service
 {
-    public class NotificationService : INotificationService
+    public class NotificationService : Interface.INotificationService
     {
         private readonly IEmailService _emailService;
+        private readonly IUserService _userService;
 
-        public NotificationService(IEmailService emailService)
+        public NotificationService(IEmailService emailService, IUserService userService)
         {
             _emailService = emailService;
+            _userService = userService;
         }
 
         public void SendEnquiry(ContactFormViewModel contactVM)
         {
+            int totalRecords;
+            var users = _userService.GetAll(0, 20, out totalRecords);
+            var recipients = string.Empty;
+
+            foreach (var user in users)
+            {
+                recipients += $"{user.Email};";
+            }
+
             //Generate an email message object to send
             MailMessage email = new MailMessage("no-reply@ste-pam.com", "spammenter@live.com");
             email.Subject = "Contact Form Request";
