d3d69a031fb837f726f2cd6e4744f719c13b005d
Ste Pammenter <spammenter@live.com>
2018-11-13 21:21:25 +0000

    Added old article finder.

---
+++ Tranquility.CMS/ContentFinder/OldArticleFinder.cs
@@ -0,0 +1,25 @@
+﻿using Umbraco.Core;
+using Umbraco.Web.Routing;
+
+namespace Tranquility.ContentFinder
+{
+    public class OldArticleFinder : IContentFinder
+    {
+        public bool TryFindContent(PublishedContentRequest contentRequest)
+        {
+            var path = contentRequest.Uri.GetAbsolutePathDecoded();
+            path = path.Replace("/article", "");
+            var contentCache = contentRequest.RoutingContext.UmbracoContext.ContentCache;
+            var content = contentCache.GetByRoute(path);
+            if (content == null)
+            {
+                return false;
+            }
+            else
+            {
+                contentRequest.PublishedContent = content;
+                return true;
+            };
+        }
+    }
+}
--- Tranquility.CMS/GlobalEventHandler.cs
+++ Tranquility.CMS/GlobalEventHandler.cs
@@ -2,12 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Tranquility.ContentFinder;
 using Umbraco.Core;
+using Umbraco.Web.Routing;
 
 namespace Tranquility
 {
     public class GlobalEventHandler : ApplicationEventHandler
     {
+
+        protected override void ApplicationStarting(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            base.ApplicationStarting(umbracoApplication, applicationContext);
+            ContentFinderResolver.Current.InsertTypeBefore<ContentFinderByNotFoundHandlers, OldArticleFinder>();
+        }
+
         protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
         {
             DependencyRegistrar.Register(applicationContext);
