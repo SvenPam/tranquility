42d585b57487e18edc0d8653e0edf2944e671a77
Ste Pammenter <spammenter@live.com>
2018-11-17 15:49:05 +0000

    Replaced content finder with URL rewrite, cos I'm now a pro a writing them.

--- Tranquility.CMS/GlobalEventHandler.cs
+++ Tranquility.CMS/GlobalEventHandler.cs
@@ -1,22 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using Tranquility.ContentFinder;
-using Umbraco.Core;
-using Umbraco.Web.Routing;
+﻿using Umbraco.Core;
 
 namespace Tranquility
 {
     public class GlobalEventHandler : ApplicationEventHandler
     {
-
-        protected override void ApplicationStarting(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
-        {
-            base.ApplicationStarting(umbracoApplication, applicationContext);
-            ContentFinderResolver.Current.InsertTypeBefore<ContentFinderByNotFoundHandlers, OldArticleFinder>();
-        }
-
         protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
         {
             DependencyRegistrar.Register(applicationContext);
